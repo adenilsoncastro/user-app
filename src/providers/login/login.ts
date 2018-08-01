c5c12b5d7a4aecfff9ac6d88aedfbb5fe59def11
oluis <o.luisfernando@outlook.com>
2018-08-01 20:56:27 -0300

    aa

--- src/providers/login/login.ts
+++ src/providers/login/login.ts
@@ -11,8 +11,8 @@ export class LoginProvider {
   constructor(public http: HttpClient) { }
 
   login(login: LoginModel) {
-    return this.http.post<any>('http://ec2-54-218-220-67.us-west-2.compute.amazonaws.com:8080/users/login', { username: login.username, password: login.password, usertype: login.usertype });
-    //return this.http.post<any>('http://localhost:8080/users/login', { username: login.username, password: login.password, usertype: login.usertype });
+    // return this.http.post<any>('http://ec2-54-218-220-67.us-west-2.compute.amazonaws.com:8080/users/login', { username: login.username, password: login.password, usertype: login.usertype });
+    return this.http.post<any>('http://localhost:8080/users/login', { username: login.username, password: login.password, usertype: login.usertype });
   }
   
 
